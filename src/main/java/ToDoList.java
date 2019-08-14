ed116763db609e0746d542ab2d2ca6108ac66bb4
ooimingsheng <ooimingsheng@gmail.com>
2019-08-14 22:55:43 +0700

    Level-6: Delete

--- src/main/java/ToDoList.java
+++ src/main/java/ToDoList.java
@@ -21,18 +21,18 @@ public class ToDoList {
         System.out.println("Now you have " + toDoList.size() + " tasks in the list.");
     }
 
-    public void checkOffTask(int taskNo) {
-        Task task = toDoList.get(taskNo - 1);
-        task.checkOff();
-        System.out.println("Nice! I've marked this task as done:");
-        System.out.println(indent + task);
-    }
-
     public void removeTask(int taskNo) {
         Task task = toDoList.remove(taskNo - 1);
         System.out.println("Noted. I've removed this task:");
         System.out.println(indent + task);
         System.out.println("Now you have " + toDoList.size() + " tasks in the list.");
+    }
+
+    public void checkOffTask(int taskNo) {
+        Task task = toDoList.get(taskNo - 1);
+        task.checkOff();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(indent + task);
     }
 
     public void listTasks() {
