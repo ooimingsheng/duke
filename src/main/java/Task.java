0e7562daeaea98751e1ebc0b9e87629bf39558b9
ooimingsheng <ooimingsheng@gmail.com>
2019-08-14 21:26:53 +0700

    Level-4: ToDo, Event, Deadline

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,6 +2,24 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+    public static Task taskFactory(String description, String taskType){
+        if (taskType.equals("todo")) {
+            return new ToDo(description);
+        } else if (taskType.equals("deadline")){
+            String[] str = description.split(" /by ", 2);
+            String taskDescription = str[0];
+            String by = str[1];
+            return new Deadline(taskDescription, by);
+        } else if (taskType.equals("event")) {
+            String[] str = description.split(" /at ", 2);
+            String taskDescription = str[0];
+            String at = str[1];
+            return new Event(taskDescription, at);
+        } else {
+            return null;
+        }
+    }
+
     public Task(String description) {
         this.description = description;
         this.isDone = false;
