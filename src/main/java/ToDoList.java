b06f420528f5b9dd62282737857389084ba71aad
ooimingsheng <ooimingsheng@gmail.com>
2019-08-14 22:20:36 +0700

    Level-5: Handle Errors

--- src/main/java/ToDoList.java
+++ src/main/java/ToDoList.java
@@ -1,9 +1,9 @@
-import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
 
 public class ToDoList {
     private List<Task> toDoList;
+    private static final String indent = "  ";
 
     public ToDoList() {
         toDoList = new ArrayList<>();
@@ -17,7 +17,7 @@ public class ToDoList {
         Task task = Task.taskFactory(description, taskType);
         toDoList.add(task);
         System.out.println("Got it. I've added this task:");
-        System.out.println("  " + task);
+        System.out.println(indent + task);
         System.out.println("Now you have " + toDoList.size() + " tasks in the list.");
     }
 
@@ -25,14 +25,21 @@ public class ToDoList {
         Task task = toDoList.get(taskNo - 1);
         task.checkOff();
         System.out.println("Nice! I've marked this task as done:");
-        System.out.println("  " + task);
+        System.out.println(indent + task);
+    }
+
+    public void removeTask(int taskNo) {
+        Task task = toDoList.remove(taskNo - 1);
+        System.out.println("Noted. I've removed this task:");
+        System.out.println(indent + task);
+        System.out.println("Now you have " + toDoList.size() + " tasks in the list.");
     }
 
     public void listTasks() {
         int counter = 1;
         System.out.println("Here are the tasks in your list:");
         for (Task task : toDoList) {
-            System.out.println(counter + ". " + task);
+            System.out.println(counter + "." + task);
             counter++;
         }
     }
