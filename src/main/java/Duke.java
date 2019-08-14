240ebc7926d0b9fc8b0f7ef5d4078942ee7cc90a
ooimingsheng <ooimingsheng@gmail.com>
2019-08-14 22:20:36 +0700

    Level-5: Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,7 +11,7 @@ public class Duke {
 
         System.out.println(
                 "Hello! I'm Duke" + "\n"
-                + "What can I do for you?" + "\n"
+                + "What can I do for you?"
         );
 
         Scanner scanner = new Scanner(System.in);
@@ -22,20 +22,32 @@ public class Duke {
             String input = scanner.nextLine();
             String[] str = input.split(" ", 2);
             String action = str[0];
-            if (action.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                shouldContinue = false;
-            } else if(action.equals("list")) {
-                toDoList.listTasks();
-            } else if(action.equals("done")) {
-                int taskNo = Integer.parseInt(str[1]);
-                toDoList.checkOffTask(taskNo);
-            } else if(action.equals("todo") || action.equals("deadline") || action.equals("event")) {
-                String taskType = str[0];
-                String taskDescription = str[1];
-                toDoList.addTask(taskDescription, taskType);
-            } else {
-                toDoList.addTask(input);
+            try {
+                if (action.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    shouldContinue = false;
+                } else if(action.equals("list")) {
+                    toDoList.listTasks();
+                } else if(action.equals("blah")) {
+                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                } else if(action.equals("delete")) {
+                    int taskNo = Integer.parseInt(str[1]);
+                    toDoList.removeTask(taskNo);
+                } else if(action.equals("done")) {
+                    int taskNo = Integer.parseInt(str[1]);
+                    toDoList.checkOffTask(taskNo);
+                } else if(action.equals("todo") || action.equals("deadline") || action.equals("event")) {
+                    String taskType = str[0];
+                    if (str.length <= 1) {
+                        throw new DukeException("☹ OOPS!!! The description of a " + taskType + " cannot be empty.");
+                    }
+                    String taskDescription = str[1];
+                    toDoList.addTask(taskDescription, taskType);
+                } else {
+                    toDoList.addTask(input);
+                }
+            } catch (DukeException dukeException) {
+                System.out.println(dukeException.getMessage());
             }
         }
     }
